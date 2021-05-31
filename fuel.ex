7a757ce63186d6880d759b3ecacc33efb28e1906
Forestriver <vitaly.romas1@gmail.com>
2021-05-31 12:16:04 +0300

    input added

--- fuel.ex
+++ fuel.ex
@@ -1,2 +1,4 @@
-IO.puts("The amount of fuel for launch: ")
-IO.puts("The amount of fuel for landing: ")
+launch = IO.gets("Please, enter the amount of fuel for launch: ")
+landing = IO.gets("Please enter the amount of fuel for landing: ")
+IO.puts("The amount of fuel for launch: " <> launch)
+IO.puts("The amount of fuel for landing: " <> landing)
